94b4cdc09c96eccda72e112dfde0e7972ad79048
smoll <smollah@theorchard.com>
2015-06-11 14:50:49 -0400

    Fix links by prepending pasted URL with scheme

--- linklog.js
+++ linklog.js
@@ -23,6 +23,13 @@ if(Meteor.isClient){
         'submit form': function(){
             event.preventDefault();
             var url = event.target.url.value; // targets <input name="url">
+
+            // Prepend pasted URL with scheme, cf. http://stackoverflow.com/a/3543207/3456726
+            if (!url.match(/^[a-zA-Z]+:\/\//))
+            {
+                url = 'http://' + url;
+            }
+
             Meteor.call('insertLinkData', url);
 
             event.target.url.value=''; // clear text field upon submit
